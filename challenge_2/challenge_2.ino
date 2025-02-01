ab7a01828b11f68145b3d7ae2819ace3bbef05e1
murakota <kotaro.murakami@mail.utoronto.ca>
2025-02-01 13:59:48 -0500

    Added high level behavior in comment. Code to be implemented later.

--- challenge_2/challenge_2.ino
+++ challenge_2/challenge_2.ino
@@ -10,6 +10,16 @@ void setup()
 void loop()
 {
 
+  //Black - Go straight
+  //Blue - Turn left
+  //Green - Turn right
+  //Red - U-turn
+
+  //High level idea:
+  // 1. Read the distance from the ultrasonic sensor
+  // 2. Make an if statement to check if the distance is less than 5cm.
+  // 3. If the distance is less than 5cm, check the color beneath the robot. Branch based on the color.
+
   long distance = getDistanceCM();
 
   if (distance >= 200 || distance <= 0)
