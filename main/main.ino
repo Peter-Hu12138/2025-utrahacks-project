668911fba5b2911526c6669aa9402bb689131b04
ut-akhan <abdulahadk053@gmail.com>
2025-02-01 13:17:26 -0500

    fixed color sensor pins

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,9 @@
 // Define color sensor pins
-#define S0 5
-#define S1 3
+#define S0 4
+#define S1 5
 #define S2 6
-#define S3 11
-#define sensorOut 10
+#define S3 7
+#define sensorOut 8
 
 // Variables for Color Pulse Width Measurements
 int redPW = 0;
