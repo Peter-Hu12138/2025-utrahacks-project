9360f8931767493406037ab0d509da3474e3eaef
Peter-Hu12138 <hjtwillbeok@gmail.com>
2025-02-01 15:41:31 -0500

    added challenge_2 code prototype

--- challenge_2/challenge_2.ino
+++ challenge_2/challenge_2.ino
@@ -1,5 +1,34 @@
 #include "api.h"
 
+#define moveSpeed 60
+#define SteerSpeed 50
+#define distanceThreshold 10
+#define normalTurnTime 1000
+#define UTurnTime 2*normalTurnTime
+#define endCleaningTurnTime 100
+
+void moveForward(){
+  setMotors(moveSpeed, moveSpeed);
+}
+
+void moveBackward(){
+  setMotors(-moveSpeed, -moveSpeed);
+}
+
+void turnLeft(){
+  setMotors(SteerSpeed, -SteerSpeed); // might use tuning on the best speed
+}
+
+void turnRight(){
+  setMotors(-SteerSpeed, SteerSpeed);
+}
+
+void stopMotors(){
+  setMotors(0, 0);
+}
+
+unsigned long turnStartTime = 0;
+
 void setup()
 {
   Serial.begin(9600);
@@ -9,18 +38,61 @@ void setup()
 
 void loop()
 {
-
   long distance = getDistanceCM();
+  
+  if (distance < distanceThreshold){ // close to a wall, take the instruction
+    long feedbackDistance = getDistanceCM(); // declared and intialize the latest distance var
+    turnStartTime = millis(); // restart the timer
+    setMotors(0, 0);
+    delay(50); // stop for a second to read the color properly
 
-  if (distance >= 200 || distance <= 0)
-  {
-    Serial.println("Out of range");
+    if (readBlue()){
+      // Turn left
+      while (feedbackDistance <= distance * 1.5 + 4 || millis() - turnStartTime <= normalTurnTime) {
+        feedbackDistance = getDistanceCM();
+        turnLeft();
+      }
+      unsigned int temp = millis();
+      while (millis() - temp <= endCleaningTurnTime){
+        turnLeft();
+      }
+    }
+    else if (readGreen()) {
+      // Turn right
+      while (feedbackDistance <= distance * 1.5 + 4 || millis() - turnStartTime <= normalTurnTime)) {
+        feedbackDistance = getDistanceCM();
+        turnRight();
+      }
+      unsigned int temp = millis();
+      while (millis() - temp <= endCleaningTurnTime){
+        turnRight();
+      }
+    }
+    else if (readRed()) {
+      // U-turn
+      while (millis() - turnStartTime <= UTurnTime) {
+        feedbackDistance = getDistanceCM();
+        turnRight();
+      }
+      unsigned int temp = millis();
+      while (millis() - temp <= endCleaningTurnTime){
+        turnRight();
+      }
+    }
+    else if (readBlack()){
+      // go straight
+      while (millis() - turnStartTime <= 100) {
+        feedbackDistance = getDistanceCM();
+        turnRight();
+      }
+    }
+    else {
+      // default
+    }
+    stopMotors();
+    delay(50);
   }
-  else
-  {
-    Serial.print(distance);
-    Serial.println(" cm");
+  else{
+    moveForward();
   }
-
-  delay(500);
 }
