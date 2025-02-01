60d9fc39709f95cb2db9218a84850b36916535f4
Peter-Hu12138 <hjtwillbeok@gmail.com>
2025-02-01 12:45:39 -0500

    added a header file for general reusable api

---
+++ api.h
@@ -0,0 +1,24 @@
+float getColor()
+{
+    return 0.0; // TODO should be changed
+}
+
+void setMotors(int leftMotorSpeed, int rightMotorSpeed)
+{
+    // TODO
+}
+
+float getUltrasonicDistance(int leftMotorSpeed, int rightMotorSpeed)
+{
+    return 0.0; // TODO changed
+}
+
+void adjustServo()
+{
+    // TODO
+}
+
+float getServoAngle()
+{
+    return 0.0; // TODO
+}
