706812c5d1c1f50ac69382d1c7c7b790b01d0236
MiikaMatias <miikapiiparinen24@gmail.com>
2025-02-01 13:38:55 -0500

    Refactor code

---
+++ challenge_1/api.h
@@ -0,0 +1,75 @@
+#define trigPin 13
+#define echoPin 12
+
+float getColor()
+{
+    return 0.0; // TODO should be changed
+}
+
+void setMotors(int leftMotorSpeed, int rightMotorSpeed)
+{
+    // TODO
+}
+
+float getDistanceCM() // CM
+{
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    long duration = pulseIn(echoPin, HIGH);
+    long distance = (duration / 2) / 29.1; // Convert to centimeters
+    return distance;
+}
+
+void adjustServo()
+{
+    // TODO
+}
+
+float getServoAngle()
+{
+    return 0.0; // TODO
+}
+
+// Function to read Red Pulse Widths
+int getRedPW() {
+	// Set sensor to read Red only
+	digitalWrite(S2,LOW);
+	digitalWrite(S3,LOW);
+	// Define integer to represent Pulse Width
+	int PW;
+	// Read the output Pulse Width
+	PW = pulseIn(sensorOut, LOW);
+	// Return the value
+	return PW;
+}
+
+// Function to read Green Pulse Widths
+int getGreenPW() {
+	// Set sensor to read Green only
+	digitalWrite(S2,HIGH);
+	digitalWrite(S3,HIGH);
+	// Define integer to represent Pulse Width
+	int PW;
+	// Read the output Pulse Width
+	PW = pulseIn(sensorOut, LOW);
+	// Return the value
+	return PW;
+}
+
+// Function to read Blue Pulse Widths
+int getBluePW() {
+	// Set sensor to read Blue only
+	digitalWrite(S2,LOW);
+	digitalWrite(S3,HIGH);
+	// Define integer to represent Pulse Width
+	int PW;
+	// Read the output Pulse Width
+	PW = pulseIn(sensorOut, LOW);
+	// Return the value
+	return PW;
+}
+
--- challenge_1/challenge_1.ino
+++ challenge_1/challenge_1.ino
@@ -4,6 +4,7 @@
 #define S2 6
 #define S3 7
 #define sensorOut 8
+#include <api.h>
 
 // Variables for Color Pulse Width Measurements
 int redPW = 0;
@@ -51,44 +52,4 @@ void loop() {
 	Serial.print(greenPW);
 	Serial.print(" - Blue PW = ");
 	Serial.println(bluePW);
-}
-
-
-// Function to read Red Pulse Widths
-int getRedPW() {
-	// Set sensor to read Red only
-	digitalWrite(S2,LOW);
-	digitalWrite(S3,LOW);
-	// Define integer to represent Pulse Width
-	int PW;
-	// Read the output Pulse Width
-	PW = pulseIn(sensorOut, LOW);
-	// Return the value
-	return PW;
-}
-
-// Function to read Green Pulse Widths
-int getGreenPW() {
-	// Set sensor to read Green only
-	digitalWrite(S2,HIGH);
-	digitalWrite(S3,HIGH);
-	// Define integer to represent Pulse Width
-	int PW;
-	// Read the output Pulse Width
-	PW = pulseIn(sensorOut, LOW);
-	// Return the value
-	return PW;
-}
-
-// Function to read Blue Pulse Widths
-int getBluePW() {
-	// Set sensor to read Blue only
-	digitalWrite(S2,LOW);
-	digitalWrite(S3,HIGH);
-	// Define integer to represent Pulse Width
-	int PW;
-	// Read the output Pulse Width
-	PW = pulseIn(sensorOut, LOW);
-	// Return the value
-	return PW;
 }
