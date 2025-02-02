1ced1cf82f711faf8b6283f9991e8ff130d378c3
murakota <kotaro.murakami@mail.utoronto.ca>
2025-02-01 21:20:19 -0500

    challenge 1 mostly done probably

--- challenge_1/challenge_1.ino
+++ challenge_1/challenge_1.ino
@@ -3,130 +3,110 @@
 
 RGBv rgbValue;
 
-// 0 is red, 1 is blue, 2 is green, 3 is black, the starting state
+// States: 0 = red, 1 = blue, 2 = green, 3 = black (starting state)
 int currentState = 3;
 int previousState = 3;
-int tempState = 3;
+int liveReading = 3;
 
-// counter to check how far we've progressed in the circle
+// Counter to track progress through the rings
 int ringCounter = 5;
 
-// stores the original numbers for each color (default)
-int origRed = -1;
-int origBlue = -1;
-int origGreen = -1;
+// Original color reference values
+int origRed;
+int origBlue;
+int origGreen;
 
-// Delta used to detect entry into circle
+// Threshold to detect entry into the circle
 int enterCircleThreshold = 200;
 bool enteredCircle = false;
 
 int rotationSpeedBonus = 5;
-int rotationSpeedIncrement = 5;'
+int rotationSpeedIncrement = 5;
 
 int cruiseSpeed = 100;
 int rotateSpeed = 70;
 
 void setup() {
-	//Initializing the color sensor pins as output
-	pinMode(S0, OUTPUT);
-	pinMode(S1, OUTPUT);
-	pinMode(S2, OUTPUT);
-	pinMode(S3, OUTPUT);
+    // Initialize color sensor pins
+    pinMode(S0, OUTPUT);
+    pinMode(S1, OUTPUT);
+    pinMode(S2, OUTPUT);
+    pinMode(S3, OUTPUT);
 
-	//Initializing the motor pins as output
-	pinMode(EN_A, OUTPUT);
-	pinMode(IN1, OUTPUT);
-	pinMode(IN2, OUTPUT);
-	pinMode(IN3, OUTPUT);
-	pinMode(IN4, OUTPUT);
-	pinMode(EN_B, OUTPUT);
+    // Initialize motor pins
+    pinMode(EN_A, OUTPUT);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(EN_B, OUTPUT);
 
-	myServo.attach(5);
+    myServo.attach(5);
 
-	digitalWrite(S0,HIGH);
-	digitalWrite(S1,LOW);
+    digitalWrite(S0, HIGH);
+    digitalWrite(S1, LOW);
 
-	pinMode(sensorOut, INPUT);
+    pinMode(sensorOut, INPUT);
 
-	getRGB(&rgbValue);
-	if (origRed == -1 && origBlue == -1 && origGreen == -1) {
-		origRed = rgbValue.redPW;
-		origBlue = rgbValue.bluePW;
-		origGreen = rgbValue.greenPW;
-	}
-
-	Serial.begin(9600);
-}
-
-
-
-void updateliveReading() {
-	// get current state (0 = red, 1 = blue, 2 = green, 3 = black)
-	getRGB(&rgbValue);
-	liveReading = getMinimum(rgbValue.redPW, rgbValue.bluePW, rgbValue.greenPW); //Live reading
-	}
-
-void boundaryCheck(){
-	//If boundary is reached (we dont know which one)
-	if (currentState != liveReading) {
-		//If the robot accidentally moved outwards
-		if (liveReading == previousState){
-			//Stop, rotate until sensor sees the right color again before resuming
-			}
-
-		//If the robot approached inwards
-		else{
-
-		}
-	// if current state has changed, then we have progressed one ring
-	if (ringCounter >= 0) {
-		ringCounter--;
-	}
-	}
+    getRGB(&rgbValue);
+	origRed = rgbValue.redPW;
+	origBlue = rgbValue.bluePW;
+	origGreen = rgbValue.greenPW;
+    Serial.begin(9600);
 }
 
 void loop() {
-	//While outside of the circle, rotate in a spiral of increasing radius
-	while (!enteredCircle) {
-		setMotors(255, 90+rotationSpeedBonus);
-		if (rotationSpeedBonus + rotationSpeedIncrement < 200) {
-			rotationSpeedBonus += rotationSpeedIncrement;
-		}
+    // Spiral search to enter the circle
+    while (!enteredCircle) {
+        setMotors(255, 90 + rotationSpeedBonus);
+        rotationSpeedBonus = min(200, rotationSpeedBonus + rotationSpeedIncrement);
 
-		getRGB(&rgbValue);
-		int delta = getDelta(&rgbValue, origRed, origBlue, origGreen);
-		if (delta > enterCircleThreshold) {
-		enteredCircle = true;
-    Serial.println("Entered Circle");
-		}
+        getRGB(&rgbValue);
+        int delta = getDelta(&rgbValue, origRed, origBlue, origGreen);
+        if (delta > enterCircleThreshold) {
+            enteredCircle = true;
+            Serial.println("Entered Circle");
+			setMotors(0, 0); // Stop
+        	delay(500);
+        }
+    }
 
-		delay(500);
-		continue;
+	// Get the live reading
+    getRGB(&rgbValue);
+    liveReading = getMinimum(rgbValue.redPW, rgbValue.bluePW, rgbValue.greenPW);
+
+    // Cruise mode
+    setMotors(cruiseSpeed, cruiseSpeed);
+
+    // Correct if accidentally steps out
+    if (liveReading == previousState) {
+        setMotors(0, 0); // Stop
+        delay(500);
+        setMotors(rotateSpeed, 0); // Rotate in place
+
+        while (liveReading == previousState) {
+			getRGB(&rgbValue);
+  		  	liveReading = getMinimum(rgbValue.redPW, rgbValue.bluePW, rgbValue.greenPW);
+        }
+        delay(50); // Delay to let it face inwards instead of tangential 
+		setMotors(0, 0); // Stop once corrected
+    }
+
+	// If the robot moves in as desired, keep moving straight, decrement counter, update previous state
+	if (liveReading != previousState && liveReading != currentState){
+		ringCounter = max(0, ringCounter - 1);
+		previousState = currentState;
 	}
 
-	updateColourStates();
+	// Update the current state (happens every loop regardless of what happens)
+	currentState = liveReading;
 
-
-
-	//Write search mode behavior here
-
-	//Cruise mode behavior
-	setMotors(cruiseSpeed, cruiseSpeed);
-
-	//When it accidentally steps out
-	if (currentState == previousState) { 
-		setMotors(0,0); //Stop before we stray further
+	// Check if the robot has completed the circle
+	if (ringCounter == 0) {
+		setMotors(0, 0); // Stop
 		delay(500);
-		setMotors(rotateSpeed,0); //Rotate on site
-		while {
-			updateColourStates();
-			if (currentState != previousState) {
-				setMotors(0,0);
-				break;
-			}
-		}
+		//Write code to back up a bit, later.
+		release(); //Release flag
+		break;
 	}
-
-
-	delay(50);
 }
